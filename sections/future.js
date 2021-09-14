8fe10c3bc6d4e7502db9de61e1c2e73faf8c17ba
Koa Shimabukuro <koashima@gmail.com>
2021-09-13 22:25:33 -1000

    reposition future img

--- sections/future.js
+++ sections/future.js
@@ -1,8 +1,7 @@
 import {
-  // Stack,
   Flex,
   // Button,
-  // Text,
+  Spacer,
   Image,
   VStack,
   useBreakpointValue,
@@ -17,15 +16,14 @@ export default function Future() {
       backgroundSize={"cover"}
       backgroundPosition={useBreakpointValue({ base: "65%", md: "center" })}
       direction={"column"}
-      justify={"flex-end"}
+      justify={"center"}
     >
       <VStack
         w={"full"}
         justify={"center"}
-        px={useBreakpointValue({ base: 4, md: 48 })}
+        px={useBreakpointValue({ base: 4, md: 24 })}
       >
-        <Image src={"/future1.svg"} objectFit="cover" />
-
+        <Image src={"/future1.svg"} objectFit="cover" mt='64' />
         {/* <Stack maxW={"2xl"} align={"flex-end"} spacing={6}>
           <Stack direction={"row"}>
             <Button
